858751d05e3384b6cbcfa5b4d89dbee8b1d10509
jeff <jeffshen.37@gmail.com>
2023-10-24 19:03:15 -0400

    frontend updates

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ pd.set_option('display.max_colwidth', None)
 app = Flask(__name__)
 
 df = pd.read_csv('cleaned_jeopardy.csv')
-print(df.head())
 
 np.random.seed(37)
 rand_index = np.random.randint(0, df.shape[0], 1)
@@ -16,17 +15,21 @@ rand_index = np.random.randint(0, df.shape[0], 1)
 def index():
     return render_template('index.html')
 
-@app.route('/game', methods = ['POST', 'GET'])
-def game():
+@app.route('/learn', methods = ['POST', 'GET'])
+def learn():
     if request.method == 'POST':
         rand_index = np.random.randint(0, df.shape[0], 1)
         current_question = df.loc[rand_index]['question']
         current_answer = df.loc[rand_index]['answer']
-        return render_template('game.html',
+        return render_template('learn.html',
                                current_question = current_question.values[0],
                                current_answer = current_answer.values[0])
     else:
-        return render_template('game.html')
+        return render_template('learn.html')
+    
+@app.route('/game', methods = ['POST', 'GET'])
+def game():
+    return render_template('game.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
