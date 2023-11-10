660817aa1da518806806acdbaaae70c5d9a936cf
jeff <jeffshen.37@gmail.com>
2023-11-09 20:53:18 -0500

    finished learn mode

--- app.py
+++ app.py
@@ -7,23 +7,31 @@ pd.set_option('display.max_colwidth', None)
 app = Flask(__name__)
 
 df = pd.read_csv('jeopardy_full_questions_with_topics.csv')
+categories = (df['topic_name'].unique())
+categories = categories[1:]
 
-np.random.seed(37)
-rand_index = np.random.randint(0, df.shape[0], 1)
+current_category = categories[0]
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_category = categories[0])
 
 @app.route('/learn', methods = ['POST', 'GET'])
 def learn():
     if request.method == 'POST':
-        rand_index = np.random.randint(0, df.shape[0], 1)
-        current_question = df.loc[rand_index]['question']
-        current_answer = df.loc[rand_index]['answer']
+        current_category = request.form.get('learn_category')
+        if not current_category:
+            current_category = categories[0]
+        cat_df = df[df['topic_name'] == current_category]
+        random_sample = cat_df.sample(n=1)
+        current_question = random_sample['question'].values[0]
+        current_answer = random_sample['answer'].values[0]
+        print(random_sample)
         return render_template('learn.html',
-                               current_question = current_question.values[0],
-                               current_answer = current_answer.values[0])
+                               categories = categories,
+                               current_category = current_category,
+                               current_question = current_question,
+                               current_answer = current_answer)
     else:
         return render_template('learn.html')
     
