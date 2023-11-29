5217ed4fda06ed10fe1eb4296e7cf95a8abd94c6
jeff <jeffshen.37@gmail.com>
2023-11-29 16:20:50 -0500

    update to wordclouds

--- app.py
+++ app.py
@@ -7,16 +7,21 @@ pd.set_option('display.max_colwidth', None)
 app = Flask(__name__)
 
 df = pd.read_csv('jeopardy_full_questions_with_topics.csv')
-categories = (df['topic_name'].unique())
+categories = np.array(df['topic_name'].unique())
 categories = categories[1:]
-clue_values = (df['clue_value'].unique())
-clue_values = clue_values[1:]
-
+clue_values = np.array(df['clue_value'].unique())
+clue_indexes = np.argsort(clue_values)
+clue_values = clue_values[clue_indexes]
 current_category = categories[0]
+current_value = clue_values[0]
 
 @app.route('/')
 def index():
     return render_template('index.html', current_category = categories[0])
+
+@app.route('/wordclouds')
+def wordclouds():
+    return render_template('wordclouds.html')
 
 @app.route('/learn', methods = ['POST', 'GET'])
 def learn():
@@ -25,7 +30,7 @@ def learn():
         current_value = request.form.get('learn_value')
         if not current_category:
             current_category = categories[0]
-            current_value = 0
+            current_value = clue_values[0]
         cat_df = df[df['topic_name'] == current_category]
         if int(current_value) == 0:
             final_df = cat_df
@@ -33,10 +38,13 @@ def learn():
             final_df = cat_df[cat_df['clue_value'] == int(current_value)]
         random_sample = final_df.sample(n=1)
 
-        # add preprocess to remove backslashes
         current_question = random_sample['question'].values[0]
         current_answer = random_sample['answer'].values[0]
-        print(random_sample)
+
+        #removing backslashes
+        current_question = current_question.replace("\\", "")
+        current_answer = current_answer.replace("\\", "")
+
         return render_template('learn.html',
                                categories = categories,
                                clue_values = clue_values,
--- test.py
+++ test.py
@@ -1,17 +1,4 @@
 import pandas as pd
 import numpy as np
-
-df = pd.read_csv('jeopardy_full_questions_with_topics.csv')
-
-categories = (df['topic_name'].unique())
-categories = categories[1:]
-current_category = 'World Talk'
-cat_df = df[df['topic_name'] == current_category]
-
-print(cat_df)
-print(df)
-
-rand_index = np.random.randint(0, cat_df.shape[0], 1)
-current_question = cat_df.loc[137]['question']
-
-print(current_question)
+import flask
+print(flask.__version__)
